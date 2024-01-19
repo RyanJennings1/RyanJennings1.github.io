48d2e5299f8cd62a1c4b379cc315258dafdf7d9d
Ryan Jennings <ryanjennings1010@gmail.com>
2024-01-19 00:06:23 +0000

    Ag cuir fheabhas ar an gclogchuar agus srl

--- assets/js/stor-focail.js
+++ assets/js/stor-focail.js
@@ -2,6 +2,8 @@ const POST_URL = "http://localhost:8080"
 // const RÁTA = 0.002934; // ráta na heaspónantúile dáilte
 const RÁTA = 0.00012; // ráta pioctha chun shampla níos fearr a fháil
 const MÉID_NA_BHFOCAL_LE_ÚSÁID = 20; // 20, 50, 100, srl.
+const moill = ms => new Promise(res => setTimeout(res, ms));
+
 let focail_an_chluiche = [];
 let uimhir_focail_thuigthe = 0;
 let uimhir_focail_fheicthe = 0;
@@ -103,7 +105,7 @@ function niThuigim() {
 }
 
 /**
- * Má bhíonn na focail uilig fheicthe, faigh an ríomh agus cuir críoch leis
+ * Má bhíonn na focail uilig feicthe, faigh an ríomh agus cuir críoch leis
  * an mbabhta nó uasdátaigh an scáileán.
  */
 async function ceistFhocalEile() {
@@ -113,11 +115,35 @@ async function ceistFhocalEile() {
     console.log('tuigthe: ', tuigthe);
     const toradh = await faighRíomh(tuigthe, focail_an_chluiche);
     console.log(`toradh: ${toradh}`);
-    // TODO: Léirigh clogchuar
-    document.getElementById('ceist-fhocal').innerText = `CRÍOCH. Do thoradh: ${toradh}`;
+    léirighToradh(toradh);
   } else {
     uasdátaighScáileán();
   }
+}
+
+/**
+ * Léirigh an clogchuar agus an toradh ar an scáileán. Cuir na cnaipí i bhfolach.
+ * 
+ * @param {number} toradh - an méid focail a aithníonn an t-úsáideoir
+ */
+async function léirighToradh(toradh) {
+  document.getElementById('bosca').style.display = "none";
+  document.getElementById('ag-ríomh').style.display = "flex";
+  // Tá cuma níos réidhe air nuair atá moill bheag ann
+  await moill(3000);
+  document.getElementById('ag-ríomh').style.display = "none";
+  document.getElementsByClassName('ag-lódáil')[0].style.display = "none";
+
+  document.getElementById('uimhir-thoradh').innerText = `${toradh} Focal`;
+  document.getElementById('toradh').style.display = "block";
+
+  // http://archive.sciendo.com/LIFIJSAL/lifijsal.2016.2.issue-2/lifijsal-2016-0008/lifijsal-2016-0008.pdf
+  const mu = 8121; // figiúr garbh don stór focal meánach
+  const sd = 5002; // figiúr garbh don stór focal diall caighdeánach
+  const z = (toradh - mu) / sd
+  const céatadán = (Math.round((poz(z) + Number.EPSILON) * 10000) / 100);
+  document.getElementById('masc').style.clipPath = `polygon(0 0, ${céatadán}% 0, ${céatadán}% 100%, 0 100%)`;
+  document.getElementById('céatadán').innerText = `${céatadán}%`;
 }
 
 /**
@@ -136,4 +162,44 @@ async function faighRíomh(known, words) {
     }
   });
   return res.json();
+}
+
+/**
+ * Foinse: https://github.com/diversen/zscore-probability/blob/main/index.js
+ * Tagann an fheidhm seo ó chód le John Walker, Gary Perlman, agus Dennis Iversen
+ * 
+ * @param {number} z - "Z-scór" le háthrú go "dóchúlacht"
+ * @return {number} an dhóchúlacht
+ */
+function poz(z) {
+
+  var Z_MAX = 6;
+  var y, x, w;
+
+  if (z == 0.0) {
+      x = 0.0;
+  } else {
+      y = 0.5 * Math.abs(z);
+      if (y > (Z_MAX * 0.5)) {
+          x = 1.0;
+      } else if (y < 1.0) {
+          w = y * y;
+          x = ((((((((0.000124818987 * w
+                   - 0.001075204047) * w + 0.005198775019) * w
+                   - 0.019198292004) * w + 0.059054035642) * w
+                   - 0.151968751364) * w + 0.319152932694) * w
+                   - 0.531923007300) * w + 0.797884560593) * y * 2.0;
+      } else {
+          y -= 2.0;
+          x = (((((((((((((-0.000045255659 * y
+                         + 0.000152529290) * y - 0.000019538132) * y
+                         - 0.000676904986) * y + 0.001390604284) * y
+                         - 0.000794620820) * y - 0.002034254874) * y
+                         + 0.006549791214) * y - 0.010557625006) * y
+                         + 0.011630447319) * y - 0.009279453341) * y
+                         + 0.005353579108) * y - 0.002141268741) * y
+                         + 0.000535310849) * y + 0.999936657524;
+      }
+  }
+  return z > 0.0 ? ((x + 1.0) * 0.5) : ((1.0 - x) * 0.5);
 }
